d90241734bf9a9238925ce9028c38a93a6f6ff9b
Anitha-Arjun <arjunmurugan@Arjuns-MacBook-Air.local>
2024-10-13 16:26:52 -0500

    first commit

---
+++ data/girlsClothing.js
@@ -0,0 +1,88 @@
+export const girlsClothing = [
+  {
+    category: Girls_Fashion,
+    product_name: "Tops",
+    product_id: 1,
+    price: "$5.00",
+    image:
+      "/Users/arjunmurugan/Desktop/2024-35-RTT/sba318_express_server/public/girls/tops.jpg",
+    product_description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at erat in nunc iaculis placerat mollis ac nibh. Sed et elementum arcu, nec semper turpis",
+    Rating: {
+      rate: 4,
+      count: 200,
+    },
+  },
+  {
+    category: Girls_Fashion,
+    product_name: "Jeans",
+    product_id: 2,
+    price: "$15.00",
+    image:
+      "/Users/arjunmurugan/Desktop/2024-35-RTT/sba318_express_server/public/girls/jean.jpeg",
+    product_description:
+      "Vivamus tincidunt urna nisl, non accumsan lacus porttitor et. Praesent auctor, tellus a sodales varius, mauris sem vulputate felis, sit amet commodo tellus nulla sed felis.",
+    Rating: {
+      rate: 3,
+      count: 500,
+    },
+  },
+  {
+    category: Girls_Fashion,
+    product_name: "Leggings",
+    product_id: 3,
+    price: "$15.00",
+    image:
+      "/Users/arjunmurugan/Desktop/2024-35-RTT/sba318_express_server/public/girls/leggings.jpeg",
+    product_description:
+      "Sed interdum sit amet quam id maximus. Aliquam commodo molestie mauris, non faucibus neque scelerisque quis.",
+    Rating: {
+      rate: 4,
+      count: 300,
+    },
+  },
+  {
+    category: Girls_Fashion,
+    product_name: "Mix & Match",
+    product_id: 4,
+    price: "$25.00",
+    image:
+      "/Users/arjunmurugan/Desktop/2024-35-RTT/sba318_express_server/public/girls/mixmatch.jpeg",
+    product_description:
+      "Nulla cursus semper hendrerit. Aliquam in vestibulum magna, ac aliquet nisl. Aenean maximus condimentum ante sit amet venenatis. Aliquam et arcu purus.",
+    Rating: {
+      rate: 4.2,
+      count: 700,
+    },
+  },
+  {
+    category: Girls_Fashion,
+    product_name: "Pajamas",
+    product_id: 5,
+    price: "$17.00",
+    image:
+      "/Users/arjunmurugan/Desktop/2024-35-RTT/sba318_express_server/public/girls/pajama.jpeg",
+    product_description:
+      "Nulla ipsum lorem, ultricies at porttitor sed, lobortis vel neque. Ut condimentum dictum porttitor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+    Rating: {
+      rate: 3.9,
+      count: 723,
+    },
+  },
+  {
+    category: Girls_Fashion,
+    product_name: "Rompers",
+    product_id: 6,
+    price: "$45.00",
+    image:
+      "/Users/arjunmurugan/Desktop/2024-35-RTT/sba318_express_server/public/girls/rompers.jpg",
+    product_description:
+      "Nullam auctor mauris magna, in pellentesque erat fermentum et. Mauris erat dui, gravida ac tellus sit amet, mattis maximus sem. Curabitur non rhoncus lorem. Donec laoreet ultrices dui ornare ultricies.",
+    Rating: {
+      rate: 3.2,
+      count: 127,
+    },
+  },
+];
+
+export default girlsClothing;
